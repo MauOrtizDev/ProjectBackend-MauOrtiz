a8cfe99a43fe4282afabfd13ed26bfa43529becb
MauOrtizDev <mauortizdev@gmail.com>
2023-06-12 14:23:27 -0500

    applying websocket

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import express from "express";
-import carts from "./routes/carts.router.js";
-import products from "./routes/products.router.js";
-import viewsRouter from "./routes/views.router.js";
 import __dirname from "./utils.js";
 import handlebars from "express-handlebars";
+import viewsRouter from "./routes/views.router.js";
+import { Server } from "socket.io";
+import carts from "./routes/carts.router.js";
+import products from "./routes/products.router.js";
 
 const app = express();
 const port = 8080;
+const httpServer = app.listen(port, () => {
+	console.log(`Server listening on http://localhost:${port}`);
+});
+
+const socketServer = new Server(httpServer);
 
 app.engine("handlebars", handlebars.engine());
 app.set('views', __dirname + '/views');
@@ -15,11 +21,22 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
+
 app.use("/api/products", products);
 app.use("/api/carts", carts);
 app.use('/', viewsRouter);
 
-app.listen(port, () => {
-	console.log(`Server listening on http://localhost:${port}`);
-});
+socketServer.on("connection", (socket) => {
+	console.log("a user connected");
+	socket.on('message', data =>{
+		console.log(data);
+	})
+
+	socket.emit('evento_socket_individual','Mensaje solo para recibir el socket');
+
+	socket.broadcast.emit('evento_para_todos_menos_elactual','explicito');
+
+	socketServer.emit('evento_para_todos','todos los sockets');
+})
+
 
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,1 +1,16 @@
 console.log('funcionando');
+
+const socket = io();
+socket.emit('message','Se está dando la comunicación');
+
+socket.on('evento_socket_individual', data =>{
+    console.log(data);
+})
+
+socket.on('evento_para_todos_menos_elactual', data =>{
+    console.log(data);
+})
+
+socket.on('evento_para_todos', data =>{
+    console.log(data);
+})
